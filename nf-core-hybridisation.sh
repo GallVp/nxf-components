db7fb043c5557cf06a49a1edbed16e282a7dffb2
Usman Rashid <usman@smme.edu.pk>
2024-07-22 11:11:49 +1200

    Simplified hybridization

--- nf-core-hybridisation.sh
+++ nf-core-hybridisation.sh
@@ -3,12 +3,11 @@
 cp_hybrid_module() {
     mkdir -p "./modules/gallvp/$1"
     cp -r "./nf-core-modules/modules/nf-core/$1/"* "./modules/gallvp/$1"
+    sed -i 's/modules_nfcore/modules_gallvp/1' "./modules/gallvp/$1/tests/main.nf.test"
 }
 
-# Modules for tests
-cp -r ./nf-core-modules/modules/nf-core/gunzip ./modules/nf-core/
-
 # Modules for hybrid subworkflows
+cp_hybrid_module "gunzip"
 cp_hybrid_module "ltrfinder"
 cp_hybrid_module "ltrharvest"
 cp_hybrid_module "ltrretriever/ltrretriever"
