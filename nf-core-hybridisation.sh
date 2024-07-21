656f67bf0572ad356fb11b4683569c8c8e59b18c
Usman Rashid <usman@smme.edu.pk>
2024-07-22 10:34:03 +1200

    Added fasta_ltrretriever_lai

--- nf-core-hybridisation.sh
+++ nf-core-hybridisation.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+cp_hybrid_module() {
+    mkdir -p "./modules/gallvp/$1"
+    cp -r "./nf-core-modules/modules/nf-core/$1/"* "./modules/gallvp/$1"
+}
+
+# Modules for tests
+cp -r ./nf-core-modules/modules/nf-core/gunzip ./modules/nf-core/
+
+# Modules for hybrid subworkflows
+cp_hybrid_module "ltrfinder"
+cp_hybrid_module "ltrharvest"
+cp_hybrid_module "ltrretriever/ltrretriever"
+cp_hybrid_module "ltrretriever/lai"
