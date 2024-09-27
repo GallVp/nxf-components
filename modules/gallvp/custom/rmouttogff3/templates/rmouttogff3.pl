7bcc9b82b7ad6926e8d8cea904a54ec7b828207b
Usman Rashid <usman@smme.edu.pk>
2024-09-27 17:04:44 +1200

    Fixed linting issues

--- modules/gallvp/custom/rmouttogff3/templates/rmouttogff3.pl
+++ modules/gallvp/custom/rmouttogff3/templates/rmouttogff3.pl
@@ -14,18 +14,18 @@ standard gff output from RepeatMasker is gff version 2.
 
 RepeatMasker "out.gff"
 
- seq1       RepeatMasker    similarity      1       1295    28.1    -       .       Target "Motif:Gypsy7-PTR_I-int" 3544 4847
+seq1       RepeatMasker    similarity      1       1295    28.1    -       .       Target "Motif:Gypsy7-PTR_I-int" 3544 4847
 
 RepeatMasker "out" file has the following format:
 
-    SW   perc perc perc  query     position in query              matching              repeat                 position in repeat
- score   div. del. ins.  sequence  begin    end          (left)   repeat                class/family       begin   end    (left)      ID
+SW   perc perc perc  query     position in query              matching              repeat                 position in repeat
+score   div. del. ins.  sequence  begin    end          (left)   repeat                class/family       begin   end    (left)      ID
 
-  4634   28.1  1.2  0.5  seq1         1     1295        (0) C Gypsy7-PTR_I-int      LTR/Gypsy           (1215)   4847    3544      1
+4634   28.1  1.2  0.5  seq1         1     1295        (0) C Gypsy7-PTR_I-int      LTR/Gypsy           (1215)   4847    3544      1
 
 After conversion to gff3:
 
- seq1       RepeatMasker    dispersed_repeat        1       1295    4634    -       .       ID=1_seq1_1_1295_Gypsy7-PTR_I-int;Name=Gypsy7-PTR_I-int;class=LTR;family=Gypsy;percDiv=28.1;percDel=1.2;percIns=0.5
+seq1       RepeatMasker    dispersed_repeat        1       1295    4634    -       .       ID=1_seq1_1_1295_Gypsy7-PTR_I-int;Name=Gypsy7-PTR_I-int;class=LTR;family=Gypsy;percDiv=28.1;percDel=1.2;percIns=0.5
 
 Notes:
 
@@ -45,35 +45,35 @@ select OUT; print OUT "##gff-version 3\n";
 my $lastqName = "";
 while ( my $line = <IN>)
 {
-	next if ($line =~ m/^$/);
-	next if ($line =~ m/(perc|score|SW)/);
-	chomp $line;
-	$line =~ s/^([ ]+)//;
-	$line =~ s/  / /g;
-	$line =~ s/ /\t/g;
-	$line =~ s/([\t]+)/\t/g;
-	my ($SWscore, $percDiv, $percDel, $percIns, $qName, $qStart, $qEnd, $left, $ori, $repeatName, $repeatClassFamily, $rStart, $rEnd, $rLeft, $rId, @junk) = split/\t/, $line;
-	($ori eq "C") and $ori = "-";
-	my $id = join("_", $rId,  $qName, $qStart, $qEnd, $repeatName);
-	my ($class, $family) = split/\//, $repeatClassFamily;
-	$class ||= "na";
-	$family ||= "na";
-	my $gff3Line = join("\t",
-		$qName,
-		"$source",
-		"$type",
-		$qStart,
-		$qEnd,
-		$SWscore,
-		$ori,
-		".",
-		"ID=$id;Name=$repeatName;class=$class;family=$family;percDiv=$percDiv;percDel=$percDel;percIns=$percIns");
-	if (($lastqName ne $qName) and ($lastqName ne ""))
-	{
-		select OUT; print OUT "###\n";
-	}
-	select OUT; print OUT "$gff3Line\n";
-	$lastqName = $qName;
+    next if ($line =~ m/^$/);
+    next if ($line =~ m/(perc|score|SW)/);
+    chomp $line;
+    $line =~ s/^([ ]+)//;
+    $line =~ s/  / /g;
+    $line =~ s/ /\t/g;
+    $line =~ s/([\t]+)/\t/g;
+    my ($SWscore, $percDiv, $percDel, $percIns, $qName, $qStart, $qEnd, $left, $ori, $repeatName, $repeatClassFamily, $rStart, $rEnd, $rLeft, $rId, @junk) = split/\t/, $line;
+    ($ori eq "C") and $ori = "-";
+    my $id = join("_", $rId,  $qName, $qStart, $qEnd, $repeatName);
+    my ($class, $family) = split/\//, $repeatClassFamily;
+    $class ||= "na";
+    $family ||= "na";
+    my $gff3Line = join("\t",
+        $qName,
+        "$source",
+        "$type",
+        $qStart,
+        $qEnd,
+        $SWscore,
+        $ori,
+        ".",
+        "ID=$id;Name=$repeatName;class=$class;family=$family;percDiv=$percDiv;percDel=$percDel;percIns=$percIns");
+    if (($lastqName ne $qName) and ($lastqName ne ""))
+    {
+        select OUT; print OUT "###\n";
+    }
+    select OUT; print OUT "$gff3Line\n";
+    $lastqName = $qName;
 }
 select OUT; print OUT "###\n";
 
